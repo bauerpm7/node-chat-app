e8fec0411b31c4be26211c6da8a2ab21c7a0dfbe
bauerpm <bauerpm@gmail.com>
2018-08-16 14:10:11 -0400

    Emit new message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,6 @@
 const socket = io(); 
 socket.on('connect', function () {
   console.log('Connected to server');
-  socket.emit('createMessage', {
-    from: 'Jen',
-    text: 'Hey this is Jen.'
-  });
 }); 
 
 socket.on('disconnect', function () {
